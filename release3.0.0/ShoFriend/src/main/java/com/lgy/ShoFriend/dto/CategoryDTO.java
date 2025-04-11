dba31a317d223427f1ade979f1de613c76a6b187
Wjyuy <zhzhwm@naver.com>
2025-04-11 15:40:22 +0900

    order_good(no_stock_ver)

---
+++ release3.0.0/ShoFriend/src/main/java/com/lgy/ShoFriend/dto/CategoryDTO.java
@@ -0,0 +1,13 @@
+package com.lgy.ShoFriend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class CategoryDTO {
+	private int id;
+	private String name;
+}
